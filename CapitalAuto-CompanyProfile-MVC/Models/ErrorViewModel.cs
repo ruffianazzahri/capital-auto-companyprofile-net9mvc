dc62d3dd99bef38246f4b7078c37198f4dbf19c2
Muhammad Ruffian Azzahri <muhammadruffianazzahri@gmail.com>
2025-08-18 20:37:46 +0700

    first commit

---
+++ CapitalAuto-CompanyProfile-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CapitalAuto_CompanyProfile_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
